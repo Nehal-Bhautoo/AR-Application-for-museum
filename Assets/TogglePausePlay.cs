b71aea1e1bf5580d92528eee20712f0e9d8ee82c
Nehal Bhautoo <nehalb34@gmail.com>
2020-11-15 15:53:30 +0000

    Added virtual button

---
+++ Assets/TogglePausePlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class TogglePausePlay : MonoBehaviour, ITrackableEventHandler
+{
+
+    public GameObject videoplayer;
+    protected TrackableBehaviour mTrackableBehaviout;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        throw new System.NotImplementedException();
+    }
+}
