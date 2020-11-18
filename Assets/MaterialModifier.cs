f936d52f72171fd87de79657dca2c78a0f2060ec
Nehal Bhautoo <nehalb34@gmail.com>
2020-11-18 06:26:27 +0000

    added color changer

---
+++ Assets/MaterialModifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialModifier : MonoBehaviour {
+
+    public MeshRenderer material;
+    public Slider red;
+    public Slider green;
+    public Slider blue;
+
+    // Update is called once per frame
+    public void onEdit()
+    {
+        Color color = material.material.color;
+        color.r = red.value;
+        color.g = green.value;
+        color.b = blue.value;
+        material.material.color = color;
+        material.material.SetColor("_EmissionColor", color);
+    }
+}
