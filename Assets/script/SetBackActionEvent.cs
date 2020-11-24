2553b96eab8c8cbe6485c352bbdcd6a17d4162e9
Nehal Bhautoo <nehalb34@gmail.com>
2020-11-24 08:51:11 +0000

    Add model textures

---
+++ Assets/script/SetBackActionEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetBackActionEvent : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("MobileUI");
+            }
+        }
+    }
+}
