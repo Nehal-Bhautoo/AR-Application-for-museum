fcd5a9c8c3dc0a70be930b1af09739b2e926a26b
Nehal Bhautoo <nehalb34@gmail.com>
2020-12-08 08:15:46 +0000

    change rotate script

--- Assets/script/RotateOnTouch.cs
+++ Assets/script/RotateOnTouch.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class RotateOnTouch : MonoBehaviour
 {
-    float rotationSpeed = 0.2f;
+    public float rotationSpeed;
     // Update is called once per frame
     void Update()
     {
-        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-        float YaxisRotaion = Input.GetAxis("Mouse Y") * rotationSpeed;
        
-        transform.Rotate(axis: Vector3.down, XaxisRotation);
-        transform.Rotate(axis: Vector3.right, YaxisRotaion);
-    }
+        float XaxisRotation = Input.GetTouch(0).position.x * rotationSpeed;
+        float YaxisRotaion = Input.GetTouch(0).position.y * rotationSpeed;
+        
+        transform.RotateAround(axis: Vector3.down, XaxisRotation);
+        transform.RotateAround(axis: Vector3.right, YaxisRotaion);
+        
+    } 
 }
