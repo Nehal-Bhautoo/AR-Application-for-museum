8ccf8c2c5bc46ccb8200553bb5250e028fa8196f
Ouwesh Seeroo (Ouweshs28) <ouweshseeroo@gmail.com>
2020-12-08 07:07:25 +0000

    engine sound wip

---
+++ Assets/script/playsound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playsound : MonoBehaviour
+{
+    GameObject BPlay, BStop,Cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        BPlay = GameObject.Find("Play");
+        BStop = GameObject.Find("Stop");
+        Cube = GameObject.Find("Cube");
+
+        BPlay.SetActive(true);
+        BStop.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                if (hit.collider.tag == "Play")
+                {
+                    BPlay.SetActive(false);
+                    BStop.SetActive(true);
+                    Cube.GetComponent<AudioSource>().Play();
+
+
+                }
+                if (hit.collider.tag == "Stop")
+                {
+                    BPlay.SetActive(true);
+                    BStop.SetActive(false);
+                    Cube.GetComponent<AudioSource>().Stop();
+
+                }
+            }
+        }
+        
+    }
+}
