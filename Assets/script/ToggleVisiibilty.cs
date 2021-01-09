305a16ac12cfe24ef8bc9ccf0067789f704bbfbd
Nehal Bhautoo <nehalb34@gmail.com>
2021-01-09 17:12:53 +0000

    AR cam

---
+++ Assets/script/ToggleVisiibilty.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleVisiibilty : MonoBehaviour
+{
+    public void ToogleObjectVisibilty()
+    {
+        Renderer render = gameObject.GetComponent<Renderer>();
+        if (render.enabled)
+        {
+            render.enabled = false;
+        }
+        else {
+            render.enabled = true;
+        }
+    }
+}
