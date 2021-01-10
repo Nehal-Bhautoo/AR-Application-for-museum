b47036cf436aa6474e5725765b8a99408088db3b
Ouwesh Seeroo (Ouweshs28) <ouweshseeroo@gmail.com>
2021-01-10 18:40:07 +0000

    fixed buttons overlapping

--- Assets/script/ShowAllardInfo.cs
+++ Assets/script/ShowAllardInfo.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class ShowAllardInfo : MonoBehaviour
 {
     public GameObject CarInfo;
+    public GameObject GalleryIcon;
+    public GameObject PlayIcon;
+    public GameObject StopIcon;
 
-    public void OpenSlideShow()
+
+    public void OpenAllardInfo()
     {
         if (CarInfo != null)
         {
             bool isActive = CarInfo.activeSelf;
+            bool isGallery = GalleryIcon.activeSelf;
+            bool isSound = PlayIcon.activeSelf;
             CarInfo.SetActive(!isActive);
+            GalleryIcon.SetActive(!isGallery);
+            PlayIcon.SetActive(!isSound);
+            StopIcon.SetActive(!isSound);
+
         }
     }
 }
