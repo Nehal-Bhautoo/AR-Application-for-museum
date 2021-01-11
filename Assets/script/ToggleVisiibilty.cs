b20c1012e326305c91921c0295f167392ef0056f
Nehal Bhautoo <nehalb34@gmail.com>
2021-01-11 19:51:37 +0000

    No Comments

--- Assets/script/ToggleVisiibilty.cs
+++ Assets/script/ToggleVisiibilty.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class ToggleVisiibilty : MonoBehaviour
 {
-    public GameObject gameObjects;
+    public GameObject gameObject1;
+    public GameObject gameObject2;
+    public GameObject gameObject3;
+
+    public GameObject gameObject4;
+    public GameObject gameObject5;
+    public GameObject gameObject6;
 
     private void Update()
     {
@@ -17,14 +23,32 @@ public class ToggleVisiibilty : MonoBehaviour
         {
             if(Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                Renderer render = gameObjects.GetComponent<Renderer>();
+                Renderer render = gameObject1.GetComponent<Renderer>();
+                Renderer render2 = gameObject2.GetComponent<Renderer>();
+                Renderer render3 = gameObject3.GetComponent<Renderer>();
+
+                Renderer render4 = gameObject4.GetComponent<Renderer>();
+                Renderer render5 = gameObject5.GetComponent<Renderer>();
+                Renderer render6 = gameObject6.GetComponent<Renderer>();
                 if (render.enabled)
                 {
                     render.enabled = false;
+                    render2.enabled = false;
+                    render3.enabled = false;
+
+                    render4.enabled = false;
+                    render5.enabled = false;
+                    render6.enabled = false;
                 }
                 else
                 {
                     render.enabled = true;
+                    render2.enabled = true;
+                    render3.enabled = true;
+
+                    render4.enabled = true;
+                    render5.enabled = true;
+                    render6.enabled = true;
                 }
             }
         }
