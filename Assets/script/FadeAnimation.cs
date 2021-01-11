3840b823c6d6000a21b16bf3c15d1ba2f9b52d04
Nehal Bhautoo <nehalb34@gmail.com>
2021-01-11 17:26:53 +0000

    add animation

---
+++ Assets/script/FadeAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAnimation : MonoBehaviour
+{
+    private float fadeSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Color objectColor = this.GetComponent<Renderer>().material.color;
+        float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+        objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+        this.GetComponent<Renderer>().material.color = objectColor;
+        
+    }
+}
---
+++ Assets/script/SpinAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAnimation : MonoBehaviour
+{
+    float speed = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
