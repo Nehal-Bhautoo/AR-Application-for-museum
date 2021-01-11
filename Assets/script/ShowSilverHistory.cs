1b46bb3675b6e2e9535a0e1df61c8970f42387ea
Ouwesh Seeroo (Ouweshs28) <ouweshseeroo@gmail.com>
2021-01-11 18:42:43 +0000

    history info added

---
+++ Assets/script/ShowSilverHistory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSilverHistory : MonoBehaviour
+{
+    public GameObject CarInfo;
+    
+
+
+    public void OpenAllardInfo()
+    {
+        if (CarInfo != null)
+        {
+            bool isActive = CarInfo.activeSelf;
+            CarInfo.SetActive(!isActive);
+
+        }
+    }
+}
